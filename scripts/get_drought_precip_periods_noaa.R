7f593b1790825914114f38cc5c570f5cce9f2e74
Ryan Peek <rapeek@ucdavis.edu>
2022-12-30 11:29:56 -0800

    Update code and plotting for precip

--- scripts/get_drought_precip_periods_noaa.R
+++ scripts/get_drought_precip_periods_noaa.R
@@ -19,7 +19,7 @@ library(ggdark)
 # list of counties
 cntys <- list_counties("CA")
 
-county_sel <- c("Sacramento", "El Dorado")
+county_sel <- c("Kern", "Sacramento", "El Dorado", "Sonoma")
 cnty <- filter(cntys, county %in% county_sel) %>%
   pull(county_code)
 
@@ -37,7 +37,6 @@ interval <- 24
 make_url <- glue("https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/county/time-series/CA-{cnty}/pcp/{interval}/{mon}/{yr_start}-{yr_end}.csv?base_prd=true&begbaseyear={yr_start}&endbaseyear={yr_end}&filter=true&filterType=binomial")
 
 # use url
-#dat_path <- GET(url=make_url)
 dat_out <- read_csv(make_url, skip = 4, id = "path")
 dat_out <- dat_out %>%
   mutate(county_code = substr(gsub("https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/county/time-series/", "", path), 4,6)) %>%
@@ -48,5 +47,12 @@ dat_out <- dat_out %>%
   mutate(date = ymd(paste(Date,"01")))
 
 # plot
-ggplot() + geom_line(data=dat_out, aes(x=date, y=Anomaly))
+ggplot() + geom_line(data=dat_out, aes(x=date, y=Anomaly, color=county)) +
+  ggdark::dark_theme_classic(base_family = "Roboto Condensed") +
+  scale_x_date(date_breaks = "years", date_labels = "%Y")+
+  theme(axis.text.x = element_text(angle=60, hjust=1))+
+  scale_color_brewer("County", type = "qual", palette = "Dark2")+
+  labs(title = glue("Precip Anomaly for {month.abb[{mon}]}: Aggegated over {interval} months"),
+       x="", y="Precip Anomaly",
+       caption="Data source: Precipitation from <www.ncei.noaa.gov/access/monitoring/climate-at-a-glance>")
 
