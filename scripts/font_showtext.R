b0a249ed7f33dd933b210daa93ce6deda9e5b973
Ryan Peek <rapeek@ucdavis.edu>
2023-02-11 16:10:41 -0800

    update fonts

--- scripts/font_showtext.R
+++ scripts/font_showtext.R
@@ -12,8 +12,44 @@ font_paths()
 # add a new path
 # font_paths(new = r"(C:\Users\RPeek\AppData\Local\Microsoft\Windows\Fonts)")
 
-# pick a font
-fnt_to_add <- "Titillium" # Caveat
+# Pick a Font -------------------------------------------------------------
+
+# pick a font pair
+fnt_headers <- "PT Sans"
+fnt_text <- "Noto Sans"
+
+# PT Sans: header text
+# Noto Sans: body text
+
+fnt_headers <- "Merriweather"
+fnt_text <- "Source Sans Pro"
+# Merriweather: good for headings, not for small
+# Source Sans Pro: good for smaller text
+
+fnt_headers <- "Lora"
+fnt_text <- "Libre Franklin"
+# Lora: Sans Serif for labeling, good for header and subheader
+# Libre Franklin: Serif for reading, chart text, good for callouts
+
+fnt_headers <- "Roboto"
+fnt_text <- "Roboto Condensed"
+# Roboto: good for header and subheader
+# Roboto Condensed: very good for small labeling
+
+# same!?
+fnt_headers <- "Assistant"
+fnt_text <- "Assistant"
+
+# same!?
+fnt_headers <- "Atkinson Hyperlegible"
+fnt_text <- "Atkinson Hyperlegible"
+
+# FUN
+# Caveat: cursive fun, best for larger headers
+# Schoolbell: scrawl
+
+
+# Look Locally ------------------------------------------------------------
 
 # search for a font name or family
 font_files() %>% filter(grepl(fnt_to_add, ps_name))
@@ -27,23 +63,33 @@ font_files() %>% filter(grepl(fnt_to_add, family))
 # add locally
 font_add(family = fnt_to_add, regular = glue("{ttf}"))
 
+
+# Look on Web -------------------------------------------------------------
+
 # add straight from google
-font_add_google(fnt_to_add)
+font_add_google(fnt_headers)
+font_add_google(fnt_text)
+
+# Plot --------------------------------------------------------------------
 
 # call this for all future plots, else _begin and _end
 showtext_auto()
 
-# tst
+# tst plot
 ggplot() +
   geom_point(data=penguins, aes(x=body_mass_g, y=flipper_length_mm, fill=species), alpha=0.8, pch=21, size=3, color=alpha("gray80", 0.5)) +
-  ggdark::dark_theme_classic(base_family = fnt_to_add, base_size = 14) +
+  ggdark::dark_theme_classic(base_family = fnt_headers) +
   scale_fill_manual("Penguin Species", values = c("darkorange","purple","cyan4")) +
-  labs(title="Palmer Penguins Data", y="Flipper Length (mm)", x="Body Mass (g)")
+  labs(title="Palmer Penguins Data", y="Flipper Length (mm)", x="Body Mass (g)") +
+  theme(title = element_text(face="bold", size=22),
+        axis.text = element_text(family=fnt_text, size = 14),
+        legend.text = element_text(family=fnt_text, size = 16))
 
 
 # Can we Export? ----------------------------------------------------------
 
-ggsave(glue::glue("figs/penguins_example_w_fonts_{fnt_to_add}.png"),
+ggsave(glue::glue("figs/penguins_example_w_fonts_{fnt_headers}.png"),
        width = 8, height = 6, units="in", dpi=300)
-ggsave(glue::glue("figs/penguins_example_w_fonts_{fnt_to_add}.pdf"),
-       width = 8, height = 6, units="in", device = cairo_pdf)
+
+#ggsave(glue::glue("figs/penguins_example_w_fonts_{fnt_to_add}.pdf"),
+#       width = 8, height = 6, units="in", device = cairo_pdf)
