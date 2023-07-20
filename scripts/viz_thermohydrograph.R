c514c22bb14f5fbd8822429c283aaa1739ffe843
Ryan Peek <rapeek@ucdavis.edu>
2023-07-20 16:26:28 -0700

    update thermohydrograph

--- scripts/viz_thermohydrograph.R
+++ scripts/viz_thermohydrograph.R
@@ -1,38 +1,52 @@
 # thermohydrograph
+library(tidyverse)
+library(dataRetrieval)
+library(viridis)
+library(janitor)
+library(gridExtra)
+
+# Get Data ----------------------------------------------------------------
+
+siteNumber <- "01491000"
+parameterCd <- c("00010", "00060") # Temperature and discharge
+statCd <- c("00003") # Mean and maximum
+startDate <- "2022-10-01"
+endDate <- "2023-09-30"
+
+dff <- readNWISdv(siteNumber, parameterCd,
+                  startDate, endDate,
+                  statCd = statCd)
+
+dff <- readNWISuv(siteNumber, parameterCd,
+                  startDate, endDate)
+
+dff <- dataRetrieval::addWaterYear(dff)
+dff <- dataRetrieval::renameNWISColumns(dff)
+
+dff <- clean_names(dff)
+dff$datetime <- dff$date
+
+dff <- filter(dff, !is.na(wtemp))
 
 # Make Test Thermohydro Plots --------------------------------------------------------------
 
 ## SET COLORS AND BREAKS FOR WATER TEMPERATURES
-breaksair<-(seq(0,36,4)) # best for air
-breaksh20<-(seq(0,27,3)) # best for water
+#breaks<-(seq(0,36,4)) # best for air
+breaks<-(seq(0,27,3)) # best for water
 
 palette<-c("black","midnightblue","blue","deepskyblue2",
                   "green4","green","yellow","orange","red","darkviolet") # orange orangered brown4
 
-# USGS plot with air as thermoscale
-
 ## plot stage or flow
-water<-"flow_cms" #  "stage_m"
-usgslab<-"Q (cms)" # "Stage (m)"
-minusgs<-min(dff$flow_cms) # or stage_m
+usgslab<-"Q (cfs)" # "Stage (m)"
+minusgs<-min(dff$flow) # or stage_m
 
-grid.arrange(
-  ggplot() + # can switch flow_cms with stage_m and use air for color
-    geom_line(data = dff, aes_string(x = "datetime", y = water), color = "grey50", size = 1, alpha=0.8) +
-    scale_colour_gradientn(name=expression(paste("AirTemp(",degree,"C)")),colours=palette(palette), values=breaksair,
-                           rescaler = function(x, ...) x, oob = identity,limits=range(breaksair), breaks=breaksair, space="Lab") +
-    geom_ribbon(data = dff[dff$datetime <= dff$datetime[nrow(dff)],],
-                aes_string(x = "datetime", ymax = water, ymin = minusgs), fill = "gray80", alpha = .5) +
-    geom_line(data = dff[dff$datetime <= dff$datetime[nrow(dff)],],
-              aes_string(x = "datetime", y = water, color="air_C"), size = 1) +
-    geom_point(data = dff[dff$datetime == dff$datetime[nrow(dff)],],
-               aes_string(x = "datetime", y = water), pch=21, fill="gray10", col="white",size = 8) +
-    labs(list(x="",y = usgslab)) + theme_bw() +
-    theme(axis.text.x = element_text(face="bold", size=12),
-          axis.text.y = element_text(face="bold", size=12),
-          axis.title.y = element_text(face="bold", size=12),
-          panel.background = element_rect(fill = "transparent",colour = NA),
-          panel.grid.minor = element_blank(),
-          panel.grid.major = element_blank(),
-          plot.background = element_rect(fill = "transparent",colour = NA))
-)
+ggplot() + geom_line(data = dff, aes(x=date, y=flow, color=wtemp), size=2) +
+  scale_color_viridis_c(alpha = 0.8, name=expression(paste("Water \nTemp (", degree, "C)")), option = "A")+
+  #scale_color_gradientn(name=expression(paste("WaterTemp(",degree,"C)")),
+  #                       colors=palette(palette), values = breaks, limits = range(breaks),
+  #                       rescaler = function(x, ...) x) +
+  #geom_ribbon(data = dff[dff$date <= dff$date[nrow(dff)],],
+  #            aes(x = date, ymax = flow, ymin = minusgs), fill = "gray80", alpha = .5) +
+  labs(title = "Thermohydrograph", x="",y = usgslab) + hrbrthemes::theme_ipsum_rc()
+
