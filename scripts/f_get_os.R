21cb5d52db91266af0b944f489c130dd60b43245
Ryan Peek <rapeek@ucdavis.edu>
2023-09-13 08:32:43 -0700

    update the os path

--- scripts/f_get_os.R
+++ scripts/f_get_os.R
@@ -1,6 +1,19 @@
-get_ospath <- function() {
+
+# use this to check the operating system
+f_get_os <- function() {
+  if (.Platform$OS.type == "windows") {
+    "windows"
+  } else if (.Platform$OS.type == "unix") {
+    "unix"
+  } else {
+    stop("Unknown OS")
+  }
+}
+
+f_get_ospath <- function() {
   if(.Platform$OS.type=="windows"){
-    (input_path <- "C://Users/RPeek/") }
+    (input_path <- "C:/Users/RPeek/") }
   else if(.Platform$OS.type=="unix"){
-    (input_path <- osx_path) }
+    (input_path <- "/Users/rapeek/") }
+  return(input_path)
 }
