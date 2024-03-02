d112d42da587b62721cbaeb96cdbe683710a0efd
Ryan Peek <rapeek@ucdavis.edu>
2024-03-01 16:57:43 -0800

    update dist to water

--- scripts/distance_to_water.R
+++ scripts/distance_to_water.R
@@ -8,7 +8,8 @@ library(nhdplusTools)
 library(tidyverse)
 library(nngeo)
 library(rmapshaper)
-
+library(mapview)
+mapviewOptions(fgb=FALSE)
 # st_nn(a, b, k = 1, returnDist = T) # returns dist and nearest
 
 
@@ -42,12 +43,12 @@ flowlines <- nhdplusTools::get_nhdplus(bbox_poly)
 
 crs <- 3310
 
-flowlines <- flowlines |>
+flowlines_simple <- flowlines |>
   st_transform(crs) |>
   rmapshaper::ms_simplify(keep=0.1) |>
   st_intersection(bbox_poly)
 
-water_bodies <-
+water_bodies_simple <-
   st_transform(water_bodies, crs) |>
   st_buffer(0) |>
   rmapshaper::ms_simplify(keep = 0.05) |>
@@ -55,50 +56,60 @@ water_bodies <-
 
 # Use NN Approach ---------------------------------------------------------
 
-nearest_coords <- st_nn(bird_pts, flowlines_sf, k = 1, returnDist = T)
+nearest_coords <- st_nn(bird_pts, flowlines_simple, k = 1, returnDist = T)
 
 # make a df
-nearest_df <- tibble("comid"=unlist(nearest_coords$nn), dist_m=unlist(nearest_coords$dist), SiteID=search$SiteID)
+nearest_df <- tibble("comid"=unlist(nearest_coords$nn), dist_m=unlist(nearest_coords$dist), SiteID=bird_pts$SiteID)
 
 # add sf
-birds_dists <- left_join(bird_pts, nearest_df)
+birds_dists2 <- left_join(bird_pts, nearest_df)
 
-mapview(birds_dists)
+# trim flowline layer
+flowline_birds <- flowlines[birds_dists2$comid,] |>
+  select(comid, gnis_id:streamorde, pathlength:arbolatesu)
 
-# Approach 1 --------------------------------------------------------------
-
-#add the coordinates for the points in SF dataframe A and B --------------
-a_coord <- st_coordinates(a)
-a <- cbind(a, a_coord)
-
-b_coord <- st_coordinates(b)
-b <- cbind(b, b_coord)
+mapview(birds_dists2, zcol="dist_m") + mapview(flowline_birds)
 
 
-#get closest feature in B to A -----------------------------------------
-A_B <- a %>%
-  st_join(b %>%
-            select(B_ID, X, Y) %>%
-            rename(B_X = X, B_Y = Y), join = st_nearest_feature)
+# Use Snapping Approach ---------------------------------------------------
+
+st_snap_points <- function(x, y, namevar, max_dist = 1000) {
+
+  # this evaluates the length of the data
+  if (inherits(x, "sf")) n = nrow(x)
+  if (inherits(x, "sfc")) n = length(x)
+
+  # this part:
+  # 1. loops through every piece of data (every point)
+  # 2. snaps a point to the nearest line geometries
+  # 3. calculates the distance from point to line geometries
+  # 4. retains only the shortest distances and generates a point at that intersection
+  out = do.call(c,
+                lapply(seq(n), function(i) {
+                  nrst = st_nearest_points(st_geometry(x)[i], y)
+                  nrst_len = st_length(nrst)
+                  nrst_mn = which.min(nrst_len)
+                  if (as.vector(nrst_len[nrst_mn]) > max_dist) return(st_geometry(x)[i])
+                  return(st_cast(nrst[nrst_mn], "POINT")[2])
+                })
+  )
+  # this part converts the data to a dataframe and adds a named column of your choice
+  out_xy <- st_coordinates(out) %>% as.data.frame()
+  out_xy <- out_xy %>%
+    mutate({{namevar}} := x[[namevar]]) %>%
+    st_as_sf(coords=c("X","Y"), crs=st_crs(x), remove=FALSE)
+
+  return(out_xy)
+}
+
+# takes a while
+snapped <- st_snap_points(bird_pts, flowlines_simple, namevar = "SiteID", 5000)
+
+mapview(snapped) + mapview(flowlines_simple)
 
 
+# Or THis -----------------------------------------------------------------
 
-#create a WKT from the coords of A and closest feature in B --------------
-A_B$line_wkt <- paste('linestring(',A_B$X,A_B$Y,',',A_B$B_X,A_B$B_Y,')')
-
-
-#Convert WKT into Geom--------------------------------------
-A_B <- A_B %>%
-  st_drop_geometry() %>%
-  st_as_sf( wkt = 'line_wkt ', crs= 4326)
-
-
-#Get the length (distance) of each line ----------------------------------
-A_B$length <- as.numeric(st_length(A_B) )
-
-
-#Join results with original A --------------------------------------------
-a <- a %>%
-  left_join(A_B %>%
-              st_drop_geometry() %>%
-              select(A_ID, B_ID, length), by = 'A_ID')
+dist_for_edge <- st_geometry(obj = polygons_sf) %>%
+  st_cast(to = 'MULTILINESTRING') %>%
+  st_distance(y=points_sf)
