e4db8546bb8c26fc3bc9dcffce232e8122f2b327
Ryan Peek <rapeek@ucdavis.edu>
2024-05-04 14:20:42 -0700

    update

--- scripts/map_night_light.R
+++ scripts/map_night_light.R
@@ -4,6 +4,7 @@ library(terra)
 library(tidyverse)
 library(hrbrthemes)
 library(rnaturalearth)
+#remotes::install_github("ropensci/rnaturalearthhires")
 library(tigris)
 library(glue)
 
@@ -14,7 +15,6 @@ library(glue)
 # from this article: https://www.nature.com/articles/s41597-020-0510-y
 # data: https://figshare.com/articles/dataset/Harmonization_of_DMSP_and_VIIRS_nighttime_light_data_from_1992-2018_at_the_global_scale/9828827/7
 r <- rast("https://figshare.com/ndownloader/files/17626016")
-
 cntry <- "Sweden"
 
 # get a country
@@ -58,7 +58,7 @@ ggsave(filename = glue("figs/lights_in_{cnty_sel}_county.png"), dpi=300,
        width = 10, height = 8)
 
 # aggegrate to coarser res
-r_xy_10 <- extract(aggregate(r2, fact = 10), v, xy = TRUE)
+r_xy_10 <- terra::extract(terra::aggregate(r2, fact = 10), v, xy = TRUE)
 r_xy_10 <- rename(r_xy_10, "val"= 2)
 
 # plot
@@ -80,7 +80,7 @@ ggsave(filename = glue("figs/lights_in_{cntry}.png"), dpi=300,
        width = 10, height = 8)
 
 # aggegrate to coarser res
-r_xy_10 <- extract(aggregate(r2, fact = 10), v, xy = TRUE)
+r_xy_10 <- terra::extract(terra::aggregate(r2, fact = 10), v, xy = TRUE)
 r_xy_10 <- rename(r_xy_10, "val"= 2)
 
 # plot
