f2f04573e9a7b18bc315f0f4b2bc30fce24aa808
Ryan Peek <rapeek@ucdavis.edu>
2024-05-06 22:59:50 -0700

    map demo

--- scripts/map_demo.R
+++ scripts/map_demo.R
@@ -6,6 +6,9 @@ library(sf)           # make spatial data
 library(mapview)      # interactive maps!
 mapviewOptions(fgb = FALSE)
 library(basemaps)
+library(tigris)
+library(terra)
+library(tidyterra)
 
 # the url for the Form data
 form_data <- paste0("https://docs.google.com/spreadsheets/d/e/",
@@ -31,20 +34,18 @@ dat_geo <- dat |> rename(latitude=y_lat, longitude=x_lon) |>
 dat_geo_ca <- dat_geo[ ca,]
 
 # set defaults
-set_defaults(map_service = "osm", map_type = "topographic")
+#set_defaults(map_service = "osm", map_type = "topographic")
+#set_defaults(map_service = "esri", map_type = "natgeo_world_map")
 set_defaults(map_service = "esri", map_type = "world_imagery")
-set_defaults(map_service = "esri", map_type = "natgeo_world_map")
 
 # use terra to crop to area of interest
-base_rast <- basemap_raster(ca, force = TRUE, verbose = FALSE,
-                            map_service = "esri",
-                            map_type = "world_imagery")
+base_rast <- basemap_raster(ca, force = TRUE, verbose = FALSE)
 base_terra <- terra::rast(base_rast) # convert to terra
 # mask to just CA
 base_terra_mask <- terra::crop(base_terra, st_transform(ca,3857),
                                mask=TRUE)
 
-# map
+# map all CA
 library(tidyterra)
 
 base_map <-
@@ -58,3 +59,41 @@ base_map <-
   theme_void()
 
 base_map
+
+# map just Sacramento
+# use terra to crop to area of interest
+cnty_rast <- basemap_raster(cnty[cnty$NAME=="Sacramento",], force = TRUE, verbose = FALSE)
+cnty_terra <- terra::rast(cnty_rast) # convert to terra
+
+# mask to just cnty
+cnty_terra_mask <- terra::crop(cnty_terra, st_transform(cnty[cnty$NAME=="Sacramento",], 3857),
+                               mask=TRUE)
+
+
+cnty_map <-
+  ggplot() +
+  # cropped only
+  geom_spatraster_rgb(data=cnty_terra_mask, alpha=0.8) +
+  geom_sf(data=dat_geo_ca[cnty[cnty$NAME=="Sacramento",],], fill="yellow", alpha=0.8, pch=21, col=alpha("gray40", 0.5), size=5) +
+  geom_sf(data=cnty[cnty$NAME=="Sacramento",], fill=alpha("gray70", 0), color="orange", lwd=1) +
+  ggspatial::annotation_north_arrow(location="br", width = unit(1.2, "cm"), pad_y = unit(1.1, "cm")) +
+  ggspatial::annotation_scale(unit_category="imperial", location="br") +
+  theme_void()
+
+cnty_map
+
+
+library(tmap)
+tmap_mode("plot")
+
+tm1 <- tm_shape(cnty_terra_mask) +
+  tm_rgb() +
+  tm_layout(frame=FALSE, title = "Places to Eat in Sacramento County", fontface = "bold", fontfamily = "Roboto Condensed", title.position = c(0.5, 0.2)) +
+  tm_compass(position = c("right","bottom"), size = 3) +
+  tm_scale_bar(position = c("right", "bottom")) +
+  tm_shape(dat_geo_ca[cnty[cnty$NAME=="Sacramento",],]) +
+  tm_sf(col="yellow", shape=21, alpha=0.8, size = 1, border.col=alpha("gray30",0.5))
+tm1
+
+tmap_mode("view")
+tm1
